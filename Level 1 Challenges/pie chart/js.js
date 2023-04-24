0352dff2d07f3174b375231384cf7d790e42e76c
Maria Kuneva <508537@student.fontys.nl>
2023-04-24 17:03:19 +0300

    challenge

--- Level 1 Challenges/pie chart/js.js
+++ Level 1 Challenges/pie chart/js.js
@@ -1,25 +1,29 @@
 let ctx = document.querySelector("canvas").getContext("2d");
 
     const results = [
-        {mood: "Netherlands", total: 799, shade: "#fcca46"},
-        {mood: "Bulgaria", total: 578, shade: "#a1c181"},
-        {mood: "Aruba", total:332, shade: "#fe7f2d"},
-        {mood: "Romania", total: 195, shade: "#619b8a"}
+        {country: "Netherlands", total: 799, shade: "#fcca46"},
+        {country: "Bulgaria", total: 578, shade: "#a1c181"},
+        {country: "Aruba", total:332, shade: "#fe7f2d"},
+        {country: "Romania", total: 195, shade: "#619b8a"}
     ];
 
     let sum = 0;
     let totalNumberOfStudents = results.reduce((sum, {total}) => sum + total, 0);
     let currentAngle = 0;
 
-    for (let moodValue of results) {
+    for (let countryValue of results) {
         //calculating the angle the slice (portion) will take in the chart
-        let portionAngle = (moodValue.total / totalNumberOfStudents) * 2 * Math.PI;
+        let portionAngle = (countryValue.total / totalNumberOfStudents) * 2 * Math.PI;
         //drawing an arc and a line to the center to differentiate the slice from the rest
-        ctx.beginPath();
-        ctx.arc(100, 100, 100, currentAngle, currentAngle + portionAngle);
-        currentAngle += portionAngle;
+        ctx.beginPath(); //starts path
+        ctx.arc(100, 100, 100, currentAngle, currentAngle + portionAngle);//creates a circular arc centered at (x, y) with
+                                                                        //  a set radius(3rd param). The path starts at startAngle(4th param), 
+                                                                        //ends at endAngle(5th param), and travels in the direction given
+                                                                        // by the last parameter in the brackets(the default is
+                                                                        // clockwise and can be skipped.)
+        currentAngle += portionAngle; 
         ctx.lineTo(100, 100);
-        //filling the slices with the corresponding mood's color
-        ctx.fillStyle = moodValue.shade;
+        //filling the slices with the corresponding country's color
+        ctx.fillStyle = countryValue.shade;
         ctx.fill();
     }
